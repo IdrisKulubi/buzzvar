daeeabfa366b83a827da74c2e9e7e993823f0238
Idris <kuulubiidris@gmail.com>
2025-05-20 12:04:24 +0300

    Enhance theming and UI components
    - Updated .npmrc to enable auto-installation of peer dependencies and set resolution mode to highest.
    - Added a new logo image for branding.
    - Refined global styles in globals.css to incorporate a gold color theme with updated color variables for light and dark modes.
    - Modified layout.tsx to utilize a new metadata construction function for improved SEO.
    - Integrated a ModeToggle component for theme switching, enhancing user experience.
    - Created a ThemeProvider component to manage theme context.
    - Added utility functions for metadata handling and string manipulation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { ModeToggle } from "@/shared/theme/mode-toggle";
+import { ModeToggle } from "@/components/shared/theme/mode-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home(){
   return (
     <div>
+    <Button>Buzzvar</Button>
       <ModeToggle />
     </div>
   )
